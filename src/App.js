d8db5ce32ef67e7ff7ecbff77af97512673955c0
manojkumar <manojkm11301@gmail.com>
2024-02-15 14:42:36 +0530

    gdf

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import MainLayout from './MainLayout/MainLayout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is sriyaan connect 12345443 2343
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < MainLayout />
     </div>
   );
 }
