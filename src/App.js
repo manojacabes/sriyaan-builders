ea459f5a3b1a2933ad9c4ae0113739f15057c36d
manojkumar <manojkm11301@gmail.com>
2024-02-15 14:11:25 +0530

    rebuild

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-// import logo from './logo.svg';
-import './App.css';
-import MainLayout from './MainLayout/MainLayout';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import TopBar from './Main/TopBar';
+import Body from './Main/Body';
+import { Container } from '@mui/material';
+import ProductDetails from './Main/ProductDetails';
+const Register = () => <div>Register Page Content</div>;
+const Login = () => <div>Login Page Content</div>;
+const OtherOption = () => <div>Other Option Page Content</div>;
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      < MainLayout />
-    </div>
+    <Router>
+      <div>
+        <TopBar />
+        <Container sx={{ height: 'calc(100vh - 64px)', overflow: 'auto' }}>
+          <Routes>
+            <Route exact path="/login" component={<Body />} />
+            <Route path="/product/:id" component={<ProductDetails />} />
+            {/* <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/other-option" component={OtherOption} /> */}
+            {/* Add more routes as needed */}
+          </Routes>
+        </Container>
+
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
